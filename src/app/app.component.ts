53829f947411fe2613bc4697b83ac6186ec3c504
r_pauloandre <r_pauloandre>
2018-02-20 01:57:03 -0300

    adicionando tela inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: string = 'HomePage';
+  rootPage: string = "HomePage";
 
   pages: Array<{title: string, component: String}>;
 
